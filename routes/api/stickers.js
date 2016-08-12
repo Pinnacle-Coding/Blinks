ddacb370dfea6be795ab33410646be1bec63ba7b
David Hacker <dmhacker@yahoo.com>
2016-08-12 15:21:46 -0700

    GET sticker

--- routes/api/stickers.js
+++ routes/api/stickers.js
@@ -1,10 +1,32 @@
+var mongoose = require('mongoose');
+var Sticker = mongoose.model('Sticker');
+
 module.exports = [
     {
-        path: '/stickers',
-        method: 'POST',
+        path: '/sticker/:id',
+        method: 'GET',
         handler: function (req, done) {
-            done(false, {
-                message: 'Connected!'
+            var query_id = req.params.id
+            var query = {
+                name: query_id
+            };
+            if (mongoose.ObjectId.isValid(query_id)) {
+                query = {
+                    _id: query_id
+                }
+            }
+            Sticker.findOne(query).exec(function (err, sticker) {
+                if (err) {
+                    done(true, {
+                        message: err.message
+                    });
+                }
+                else {
+                    done(false, {
+                        message: 'Successfully retrieved sticker',
+                        sticker: sticker
+                    });
+                }
             });
         }
     }

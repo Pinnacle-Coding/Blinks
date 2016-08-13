ff7f88007b6674b33c468435f74aa150745f8762
David Hacker <dmhacker@yahoo.com>
2016-08-12 18:52:06 -0700

    Author search by id

--- routes/api/authors.js
+++ routes/api/authors.js
@@ -1,9 +1,39 @@
+var mongoose = require('mongoose');
+var Author = mongoose.model('Author');
+
 module.exports = [
     {
         path: '/author/:id',
         method: 'GET',
         handler: function (req, done) {
-
+            var query_id = req.params.id
+            var query = {
+                $or: [
+                    {
+                        name: query_id
+                    }
+                ]
+            };
+            if (/^[0-9a-f]{24}$/.test(query_id)) {
+                query['$or'].push({
+                    _id: query_id
+                });
+            }
+            Author.findOne(query).populate({
+                path: 'packs',
+                select: 'name'
+            }).exec(function (err, author) {
+                if (err) {
+                    done(true, {
+                        message: err.message
+                    });
+                } else {
+                    done(false, {
+                        message: (author) ? 'Author found' : 'No author found',
+                        author: author
+                    });
+                }
+            });
         }
     },
     {

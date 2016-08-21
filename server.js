2c663c8651b1cbcea668023613f8dca3a718ecb1
dmhacker <dmhacker@yahoo.com>
2016-08-20 19:40:09 -0700

    Error view html

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var path = require('path');
 var app = express();
 
+// MongoDB
 var mongoose = require('mongoose');
 var Text = mongoose.model('Text', new mongoose.Schema({
     content: String,
@@ -10,27 +12,36 @@ var Text = mongoose.model('Text', new mongoose.Schema({
 mongoose.connect('mongodb://dmhacker:insaneMembrane1@ds153735.mlab.com:53735/blinks');
 require('./models/models.js');
 
+// Port
 app.set('port', (process.env.PORT || 5000));
 
+// Website directory
 app.use(express.static(__dirname + '/public'));
 
+// View engine
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+// Body parser for POST/PUT requests
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(bodyParser.json());
 
+// API route
 app.use('/api', require('./routes/api'));
+
+// 404 errors
 app.use(function (req, res) {
-    res.status(404).send('Sorry cant find that!');
+    res.status(404).sendFile(path.join(__dirname+'/views/maintenance.html'));
 });
 
+// Run server
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
 });
 
+// Run cron
 var cron = require('./cron.js');
 var timeoutCallback = function () {
     setTimeout(function () {

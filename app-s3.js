f1e1d64bbbc711b4ed0dbacadd3e766682927d5a
dmhacker <dmhacker@yahoo.com>
2016-08-30 18:35:49 -0700

    Wait for Ryan

--- app-s3.js
+++ app-s3.js
@@ -1,8 +1,8 @@
 var s3 = require('s3');
 var client = s3.createClient({
     s3Options: {
-        accessKeyId: "AKIAIOJZPC34FBRZCTGQ",
-        secretAccessKey: "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"
+        accessKeyId: "ID",
+        secretAccessKey: "KEY"
     }
 });
 

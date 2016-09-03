27d67bbc8bfdc8d8aa8a7fd15a8055ec4a385267
dmhacker <dmhacker@yahoo.com>
2016-09-03 16:17:25 -0700

    wtf

--- public/static/js/controllers/dashboard.js
+++ public/static/js/controllers/dashboard.js
@@ -2,6 +2,7 @@ app.controller('dashboardController', function ($scope, $http, Upload) {
 
     $scope.addSticker = function () {
         console.log($scope.sticker);
+        console.log($scope.file);
     };
 
 });

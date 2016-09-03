0411f02453996faf44badc6b6cff3a2a913cafc3
dmhacker <dmhacker@yahoo.com>
2016-09-03 16:25:27 -0700

    Actual uploading function

--- public/static/js/controllers/dashboard.js
+++ public/static/js/controllers/dashboard.js
@@ -1,8 +1,23 @@
 app.controller('dashboardController', function ($scope, $http, Upload) {
 
+    $scope.uploading = false;
+    
     $scope.addSticker = function () {
-        console.log($scope.sticker);
-        console.log($scope.file);
+        $scope.uploading = true;
+        Upload.upload({
+            url: '/api/stickers',
+            data: $scope.sticker,
+            method: 'POST'
+        }).then(function (resp) {
+            Materialize.toast(resp.message || 'Sticker created successfully', 4000);
+            $scope.sticker = {};
+            $scope.uploading = false;
+        }, function (resp) {
+            Materialize.toast(resp.message || 'An error occurred when creating the sticker', 4000);
+            $scope.uploading = false;
+        }, function (evt) {
+
+        });
     };
 
 });

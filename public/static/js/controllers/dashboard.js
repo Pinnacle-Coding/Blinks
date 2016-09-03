75486740eea77ace2ccb723d9a1e879101e23655
dmhacker <dmhacker@yahoo.com>
2016-09-03 16:40:52 -0700

    Finish rest of forms

--- public/static/js/controllers/dashboard.js
+++ public/static/js/controllers/dashboard.js
@@ -1,23 +1,50 @@
-app.controller('dashboardController', function ($scope, $http, Upload) {
+app.controller('dashboardController', function($scope, Upload) {
 
     $scope.uploading = false;
-    
-    $scope.addSticker = function () {
-        $scope.uploading = true;
+
+    $scope.upload = function(url, data, method, success, error) {
         Upload.upload({
-            url: '/api/stickers',
-            data: $scope.sticker,
-            method: 'POST'
-        }).then(function (resp) {
-            Materialize.toast(resp.message || 'Sticker created successfully', 4000);
-            $scope.sticker = {};
-            $scope.uploading = false;
-        }, function (resp) {
-            Materialize.toast(resp.message || 'An error occurred when creating the sticker', 4000);
-            $scope.uploading = false;
-        }, function (evt) {
+            url: url,
+            data: data,
+            method: method
+        }).then(success, error, function(evt) {
 
         });
     };
 
+    $scope.addSticker = function() {
+        $scope.uploading = true;
+        $scope.upload('/api/stickers', $scope.sticker, 'POST', function(resp) {
+            Materialize.toast(resp.message || 'Sticker created successfully', 4000);
+            $scope.sticker = {};
+            $scope.uploading = false;
+        }, function(resp) {
+            Materialize.toast(resp.message || 'An error occurred when creating the sticker', 4000);
+            $scope.uploading = false;
+        });
+    };
+
+    $scope.addPack = function() {
+        $scope.uploading = true;
+        $scope.upload('/api/packs', $scope.pack, 'POST', function(resp) {
+            Materialize.toast(resp.message || 'Pack created successfully', 4000);
+            $scope.pack = {};
+            $scope.uploading = false;
+        }, function(resp) {
+            Materialize.toast(resp.message || 'An error occurred when creating the pack', 4000);
+            $scope.uploading = false;
+        });
+    };
+
+    $scope.addAuthor = function() {
+        $scope.uploading = true;
+        $scope.upload('/api/authors', $scope.author, 'POST', function(resp) {
+            Materialize.toast(resp.message || 'Author created successfully', 4000);
+            $scope.author = {};
+            $scope.uploading = false;
+        }, function(resp) {
+            Materialize.toast(resp.message || 'An error occurred when creating the author', 4000);
+            $scope.uploading = false;
+        });
+    };
 });

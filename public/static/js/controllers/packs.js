576ed4c9eb9fe965bb0a4114adb6925f7699b1b2
dmhacker <dmhacker@yahoo.com>
2016-09-03 17:39:41 -0700

    Packs - yay

--- public/static/js/controllers/packs.js
+++ public/static/js/controllers/packs.js
@@ -1,3 +1,16 @@
 app.controller('packsController', function ($scope, $http) {
 
+    $scope.packs = [];
+    $scope.loading = true;
+
+    $http({
+        method: 'GET',
+        url: '/api/packs'
+    }).then(function (resp) {
+        $scope.packs = resp.data.packs;
+        $scope.loading = false;
+    }, function (resp) {
+        Materialize.toast(resp.data.message || 'Failed to load packs', 4000);
+        $scope.loading = false;
+    });
 });

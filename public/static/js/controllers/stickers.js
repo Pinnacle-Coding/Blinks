28f24524e2df0af22ecb3f0d53def50b148bf226
dmhacker <dmhacker@yahoo.com>
2016-09-04 09:11:11 -0700

    Pagination with no functionality

--- public/static/js/controllers/stickers.js
+++ public/static/js/controllers/stickers.js
@@ -1,10 +1,10 @@
-app.controller('stickersController', function ($scope, $http) {
+app.controller('stickersController', function($scope, $http) {
 
     $scope.stickers = [];
     $scope.loading = true;
 
     $scope.page_current = 1;
-    $scope.pagination = [1, 2, 3];
+    $scope.pagination = [1, 2, 3, 4, 5];
 
     $http({
         method: 'GET',
@@ -12,15 +12,37 @@ app.controller('stickersController', function ($scope, $http) {
         params: {
             type: 'trending'
         }
-    }).then(function (resp) {
+    }).then(function(resp) {
         $scope.stickers = resp.data.stickers;
         $scope.loading = false;
-    }, function (resp) {
+    }, function(resp) {
         Materialize.toast(resp.data.message || 'Failed to load stickers', 4000);
         $scope.loading = false;
     });
 
-    $scope.concatTags = function (sticker) {
+    $scope.nextPage = function() {
+        $scope.page_current += 1;
+        $scope.adjustPagination();
+    };
+
+    $scope.previousPage = function() {
+        if ($scope.page_current === 1) {
+            return;
+        }
+        $scope.page_current -= 1;
+        $scope.adjustPagination();
+    };
+
+    $scope.adjustPagination = function() {
+        if ($scope.page_current < 3) {
+            $scope.pagination = [1, 2, 3, 4, 5];
+        }
+        else {
+            $scope.pagination = [$scope.page_current - 2, $scope.page_current - 1, $scope.page_current, $scope.page_current + 1, $scope.page_current + 2]
+        }
+    }
+
+    $scope.concatTags = function(sticker) {
         if (!sticker) {
             return '';
         }
